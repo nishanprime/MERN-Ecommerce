0c8b8eada7db94e5e0e7b9d523d9816d2f077923
nishanprime <thapanishan9@gmail.com>
2021-04-28 14:50:21 -0400

    Created dummy local backend

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import products from './datas/product.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({
+    msg: 'Home Route called. API calling successful',
+  });
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find(p => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(5000, console.log('Server is running on port 5000'));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './Screens/HomeScreen.js';
+import { Container } from 'react-bootstrap';
+import ProductScreen from './Screens/ProductScreen.js';
+
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <Header></Header>
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+
+      <Footer></Footer>
+    </Router>
   );
 }
 
